bba26867aedc1673e48c9dde53d07efe35058334
willh <willhutchinson2.0@gmail.com>
2021-02-26 14:25:59 -0500

    finishing up steps

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 'use strict';
-const chromium = require('chrome-aws-lambda');
 require('dotenv').config();
+const chromium = require('chrome-aws-lambda');
+const latestDateSelector = 'div[id="modal_dates"] > div > div > div[class="modal-body"] > div > div:nth-child(3)';
 
 module.exports.browserTest = async (event, context, callback) => {
   let result = null;
@@ -44,7 +45,7 @@ module.exports.browserTest = async (event, context, callback) => {
   }
 
   response.body = JSON.stringify({
-    message: result
+    message: process.env.FIT4LESS_EMAIL
   });
 
   return callback(null, response);
@@ -75,9 +76,27 @@ module.exports.bookGymTime = async (event, context, callback) => {
     let page = await browser.newPage();
     await page.goto('https://myfit4less.gymmanager.com/portal/login.asp');
     
-    page.click('#emailaddress');
-    page.type('input[name=emailaddress]', )
-    page.click('#password')
+    //Enter email and password
+    await page.click('#emailaddress');
+    await page.type('input[name=emailaddress]', process.env.FIT4LESS_EMAIL, {delay: 20});
+
+    await page.click('#password');
+    await page.type('input[name=password]', process.env.FIT4LESS_PASSWORD, {delay: 20});
+
+    //Login
+    await page.click('div[id=loginButton]');
+
+    await page.waitForSelector('div[id=btn_date_select]', {visible: true});
+
+    //Select day button
+    await page.click('div[id=btn_date_select]');
+
+    //Go to newest day
+    await page.click(latestDateSelector)
+
+    //Select earliest time
+
+    //Confirm
   } catch (error) {
 
     response.statusCode = 400;
