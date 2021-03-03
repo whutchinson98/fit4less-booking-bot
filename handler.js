2014703ba4bb5fa8cbdc51af1af5eef31e7396ed
willh <willhutchinson2.0@gmail.com>
2021-03-02 19:11:06 -0500

    completed setup and removed test code

--- handler.js
+++ handler.js
@@ -8,55 +8,6 @@ const latestDateSelector = 'div[id="modal_dates"] > div > div > div[class="modal
 const mostRecentTimeSlotAvailable = 'div[class="time-slot"]:nth-child(1)';
 const confirmSelection = 'button[id="dialog_book_yes"]';
 
-module.exports.browserTest = async (event, context, callback) => {
-  let result = null;
-  let browser = null;
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: result
-    }),
-  };
-
-  try {
-    browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-    });
-    let page = await browser.newPage();
-    await page.goto('https://example.com');
-    result = await page.title();
-  } catch (error) {
-
-    response.statusCode = 400;
-
-    response.body = JSON.stringify({
-      message: error,
-      input: event
-    });
-
-    return callback(null, response);
-
-  } finally {
-    if (browser !== null) {
-      await browser.close();
-    }
-  }
-
-  response.body = JSON.stringify({
-    message: email
-  });
-
-  return callback(null, response);
-
-};
-
 module.exports.bookGymTime = async (event, context, callback) => {
   let result = null;
   let browser = null;
